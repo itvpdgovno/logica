da6985060abc0840d907b0445ae8d4a1aa1b304c
SamiDobriKot <86230447+SamiDobriKot@users.noreply.github.com>
2024-09-10 17:39:03 +0300

    Add files via upload

--- lb1a.cpp
+++ lb1a.cpp
@@ -1,27 +1,34 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-	
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
 	srand(time(NULL));
-	int size_mass, max = -1000, min = 1000;
+	int size_mass, max = -1000, min = 1000, mama=0;
 	printf("enter array z\size: ");
-	scanf("%i", &size_mass);
-	int* mass = (int*)malloc(sizeof(int) * size_mass);
-	for (int i = 0; i<size_mass; i++) {
-		*mass = rand() % 90 - 40;
-		if (*mass > max) max = *mass;
-		if (*mass < min) min = *mass;
-		printf(" %i,", *mass);
+	scanf("%i",&size_mass);
+//	int *mass = (int *)malloc(sizeof(int)*size_mass);
+	int **bigmass = (int **)malloc(sizeof(int*)*size_mass);
+	for (int i = 0;i<size_mass;i++){
+		*(bigmass+i*sizeof(int*)) = (int *)malloc(sizeof(int)*size_mass);
 	}
-	printf("\nmax: %i min: %i, diff = %i\n", max, min, max - min);
-	int a[2][10], otv;
-	for (int i = 0; i< 10; i++) {
+	for (int j = 0; j < size_mass;j++){
+		for (int i = 0; i < size_mass; i++){
+			*(*(bigmass+i*sizeof(int))+j*sizeof(int*)) = rand() % 50;
+			if (*(*(bigmass+i*sizeof(int))+j*sizeof(int*))  > max) max = *(*(bigmass+i*sizeof(int))+j*sizeof(int*)) ;
+			if (*(*(bigmass+i*sizeof(int))+j*sizeof(int*))  < min) min = *(*(bigmass+i*sizeof(int))+j*sizeof(int*)) ;
+		///	if (*(*(bigmass+i*sizeof(int))+j*sizeof(int*)) % 3 == 0 ) mama++;
+		//	printf(" %i,", *(*(bigmass+i*sizeof(int))+j*sizeof(int*)) );
+		}
+		printf("\n");
+	}
+	printf("\nmax: %i min: %i, diff = %i\n, count of mod 3", max, min, max-min,mama);
+	/*int a[2][10], otv;
+	for (int i = 0; i<10;i++){
 		a[0][i] = rand() % 100;
 		a[1][i] = rand() % 100;
 		otv = a[0][i] + a[1][i];
-		printf("%i, %i, sum = %i\n", a[0][i], a[1][i], otv);
-	}
-	free(mass);
+		printf("%i, %i, sum = %i\n", a[0][i],a[1][i], otv);
+	}*/
+	free(bigmass);
 }
